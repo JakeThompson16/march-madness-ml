e795117e3555dbb1ac6c85770a796f2163ee2041
JakeThompson16 <jake.thompson001@umb.edu>
2026-03-19 16:09:33 -0400

    implement full season trained nn

--- src/bracket/team.py
+++ src/bracket/team.py
@@ -3,11 +3,17 @@
 from functools import lru_cache
 import polars as pl
 from src.features.generate_features import generate_team_features
+from src.features.nn_features import generate_nn_feature_df
 
 
 @lru_cache(maxsize=None)
 def retrieve_features_df(season: int)->pl.DataFrame:
     df = generate_team_features(season)
+    return df
+
+@lru_cache(maxsize=None)
+def retrieve_nn_df(seasons: int)->pl.DataFrame:
+    df = generate_nn_feature_df(seasons)
     return df
 
 
@@ -28,6 +34,13 @@ class Team:
             df.filter(pl.col("team") == self.team_name)
             .drop("seed")
             .with_columns(pl.lit(seed).alias("seed"))
+        )
+
+        nn_df = retrieve_nn_df(season)
+        self.nn_df = (
+            nn_df.filter(pl.col("team_location") == self.team_name)
+            .sort("game_date")
+            .tail(1)
         )
 
     def to_dict(self) -> dict:
